579b80c4ac78b14286c3e41398dbe5d8e333223c
Siri <Siri@DESKTOP-QRUQQ5K>
2024-11-07 15:07:23 +0530

    sharing framework

---
+++ ActualFramework009/src/test/java/contactTest/Tocreatecontacttest.java
@@ -0,0 +1,12 @@
+package contactTest;
+
+public class Tocreatecontacttest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("contact created");
+
+
+	}
+
+}
---
+++ ActualFramework009/src/test/java/orgtest/Tocreateorgtest.java
@@ -0,0 +1,12 @@
+package orgtest;
+
+public class Tocreateorgtest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("org created");
+
+
+	}
+
+}
